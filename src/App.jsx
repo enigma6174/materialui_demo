3568a1ff990a791320de7a5e2708ddae55745806
enigma6174 <enigma6174@outlook.com>
2021-09-06 17:35:58 +0530

    Project First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h3>Welcome To Material UI!</h3>
+    </div>
+  );
+}
+
+export default App;
