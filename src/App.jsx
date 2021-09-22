a1e474e7a65e57545b05dccccbcb0f4d0b7505ee
enigma6174 <enigma6174@outlook.com>
2021-09-23 02:41:10 +0530

    Develop: Material UI Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import {
   AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
   Container,
   CssBaseline,
+  Grid,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
+  const classes = useStyles();
+
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -37,9 +48,62 @@ function App() {
             >
               Hello World! The Quick Brown Fox Jumps Over The Lazy Dog
             </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Primary
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image Title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      The quick brown fox jumps over the lazy dog
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Designed By enigma6174
+        </Typography>
+      </footer>
     </>
   );
 }
